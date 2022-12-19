79fa9243f74b35c16612842b3acc83c04d597eba
GoncagulKaradag <goncagul.karadag@stu.fsm.edu.tr>
2022-12-20 00:59:21 +0300

    PWA YAPILDI.

---
+++ Project/sw.js
@@ -0,0 +1,19 @@
+const CACHE ='Project'
+const FILES = ['besinler.txt','favicon.ico','index.html','JS.png','main.css','manifest.json','mobile.css','sporlar.txt','sw.js','./']
+function installCB(e) {
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache => cache.addAll(FILES))
+    .catch(console.log)
+  )
+}
+self.addEventListener('install', installCB)
+function cacheCB(e) { //cache first
+    let req = e.request
+    e.respondWith(
+      caches.match(req)
+      .then(r1 => r1 || fetch(req))
+      .catch(console.log)
+    )
+  }
+  self.addEventListener('fetch', cacheCB)
