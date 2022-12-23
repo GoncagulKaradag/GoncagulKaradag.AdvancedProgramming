d23ac71c0a0e359142de7d87a9ee1ded62a68454
GoncagulKaradag <goncagul.karadag@stu.fsm.edu.tr>
2022-12-23 12:42:36 +0300

    otomatik cache deneme

--- Project/sw.js
+++ Project/sw.js
@@ -8,12 +8,28 @@ function installCB(e) {
   )
 }
 self.addEventListener('install', installCB)
-function cacheCB(e) { //cache first
-    let req = e.request
-    e.respondWith(
-      caches.match(req)
-      .then(r1 => r1 || fetch(req))
-      .catch(console.log)
-    )
-  }
-  self.addEventListener('fetch', cacheCB)
+//function cacheCB(e) { //cache first
+ //   let req = e.request
+  //  e.respondWith(
+   //   caches.match(req)
+   //   .then(r1 => r1 || fetch(req))
+   //   .catch(console.log)
+   // )
+  //}
+ // self.addEventListener('fetch', cacheCB)
+ function save(req, resp) {
+  return caches.open(CACHE)
+  .then(cache => {
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.log)
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => { return caches.match(req).then(r1 => r1) })
+  )
+}
+self.addEventListener('fetch', fetchCB)
